f8a4a2075c353f789c0a1c24698ab2542fec5ddf
Dsimmanuel <dsimmanuel592@gmail.com>
2022-07-02 16:17:47 +0530

    search_recipe created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { SearchReceipeComponent } from './search-receipe/search-receipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRecipeComponent
+    AddRecipeComponent,
+    SearchReceipeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search-receipe/search-receipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-receipe',
+  templateUrl: './search-receipe.component.html',
+  styleUrls: ['./search-receipe.component.css']
+})
+export class SearchReceipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
